3002cf1cf3ab8037c734712a6e08650c6ca152ad
Lucas da Silva <lucas.silva00@outlook.ie>
2021-03-10 18:45:32 +0000

    GUI started. Appointment service beginning to be migrated into the GUI after testing

--- src/main/java/clinic/client/ClientGUI.java
+++ src/main/java/clinic/client/ClientGUI.java
@@ -1,7 +1,29 @@
 package clinic.client;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClientGUI {
+    private JButton appointmentServiceButton;
+    private JPanel mainPanel;
     private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton createAppointmentButton;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton loginButton;
+    private JLabel clinicAvailableServicesLabel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lucas' Clinic Services");
+        frame.setContentPane(new ClientGUI().mainPanel);
+        frame.setPreferredSize(new Dimension(1200,800));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
